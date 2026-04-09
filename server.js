41b5e3e2810564e7dec40f96390171a5c3347b68
rkchina79 <rkchina79@gmail.com>
2026-04-08 18:19:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 const SYSTEM_PROMPT = `You are Kay, a mathematics tutor and learning guide. Your role is not simply to provide answers, but to help students genuinely understand mathematics.
 
